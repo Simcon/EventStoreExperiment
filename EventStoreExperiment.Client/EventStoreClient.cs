80b9bde7dba5a9cca5dc20253f6d1211891c8d04
Paul Cook <1893351+Simcon@users.noreply.github.com>
2019-12-18 16:35:52 +0000

    First basic client tests

---
+++ EventStoreExperiment.Client/EventStoreClient.cs
@@ -0,0 +1,65 @@
+﻿using EventStore.ClientAPI;
+using EventStore.ClientAPI.Embedded;
+using EventStore.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventStoreClient
+{
+    public sealed class EventStoreClient
+    {
+        private EventStoreClient() { }
+        private static EventStoreClient instance = null;
+        private static ClusterVNode _node;
+        public static EventStoreClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var nodeBuilder = EmbeddedVNodeBuilder.AsSingleNode()
+                                  .OnDefaultEndpoints()
+                                  .RunInMemory();
+
+                    _node = nodeBuilder.Build();
+                    _node.StartAndWaitUntilReady().Wait();
+
+                    instance = new EventStoreClient();
+                }
+                return instance;
+            }
+        }
+
+        public async Task Append(string stream, string type, byte[] events)
+        {
+            using (var cn = EmbeddedEventStoreConnection.Create(_node))
+            {
+                cn.ConnectAsync().Wait();
+                await cn.AppendToStreamAsync(stream, ExpectedVersion.Any,
+                                new EventData(Guid.NewGuid(), type, true,
+                                events, null));
+
+            }
+        }
+
+        public async Task<List<ResolvedEvent>> List(string stream)
+        {
+            var streamEvents = new List<ResolvedEvent>();
+
+            StreamEventsSlice currentSlice;
+            long nextSliceStart = StreamPosition.Start;
+            using (var cn = EmbeddedEventStoreConnection.Create(_node))
+            {
+                do
+                {
+                    currentSlice = await cn.ReadStreamEventsForwardAsync(stream, nextSliceStart, 200, false);
+                    nextSliceStart = currentSlice.NextEventNumber;
+                    streamEvents.AddRange(currentSlice.Events);
+                } while (!currentSlice.IsEndOfStream);
+            }
+
+            return streamEvents;
+        }
+    }
+}
---
+++ EventStoreExperiment.Tests/ClientTests.cs
@@ -0,0 +1,24 @@
+﻿using EventStoreClient;
+using NUnit.Framework;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventStoreExperiment.Tests
+{
+    public class ClientTests
+    {
+        [Test]
+        public async Task TestWriteAndRead()
+        {
+            var client = EventStoreClient.EventStoreClient.Instance;
+
+            var stream = "stream";
+            var data = Encoding.UTF8.GetBytes("{\"Foo\":\"Bar\"}");
+
+            await client.Append(stream, "test_event", data);
+            var events = await client.List(stream);
+
+            Assert.AreEqual(events[0].Event.Data, data);
+        }
+    }
+}
